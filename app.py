4836f2a1b4ab2d7dcb135063057a569509955edc
flamingduck0 <440984240@student.sbhs.nsw.edu.au>
2022-05-12 07:38:29 +1000

    The Great Hall Page worked on, adjusted slight visuals

--- app.py
+++ app.py
@@ -2,9 +2,7 @@ from flask import Flask, render_template, request
 from costCalculator import totalCost
 import os
 
-
 app = Flask(__name__)
-
 
 @app.route('/')
 def home_page():  # put application's code here
@@ -17,12 +15,28 @@ def quotes_page():  # put application's code here
     if request.method == 'POST':
         try:
             days = int(request.form['days'])
-            cleaning = bool(request.form['cleaning'])
-            total = totalCost(days)
+            mics = int(request.form['mics'])
+            rooms = int(request.form['rooms'])
+            roomtime = int(request.form['roomtime'])
+            courts = int(request.form['courts'])
+            courttime = int(request.form['courttime'])
+            spots = int(request.form['spots'])
+            tech = int(request.form['tech'])
+            bbqtime = int(request.form['bbqtime'])
+
+
+            total = totalCost(days, mics, rooms, roomtime, courts, courttime, bbqtime, spots, tech)
             return render_template('Result.html',
                                    days=days,
-                                   cleaning=cleaning,
-                                   total=total
+                                   mics=mics,
+                                   rooms=rooms,
+                                   roomtime=roomtime,
+                                   courts=courts,
+                                   courttime=courttime,
+                                   spots=spots,
+                                   tech=tech,
+                                   bbqtime=bbqtime,
+                                   total=total,
                                    )
         except Exception:
             pass
@@ -34,25 +48,30 @@ def contact_page():  # put application's code here
     output = render_template('Contact.html')
     return output
 
+
 @app.route('/thegreathall')
 def thegreathall_page():  # put application's code here
     output = render_template('The Great Hall.html')
     return output
+
 
 @app.route('/fairlandpavilion')
 def fairlandpavilion_page():  # put application's code here
     output = render_template('Fairland Pavilion.html')
     return output
 
+
 @app.route('/theottersidecentre')
 def theottersidecentre_page():  # put application's code here
     output = render_template('The Otterside Centre.html')
     return output
 
+
 @app.route('/utsgymnasium')
 def utsgymnasium_page():  # put application's code here
     output = render_template('UTS Gymnasium.html')
     return output
+
 
 if __name__ == '__main__':
     app.run()
--- costCalculator.py
+++ costCalculator.py
@@ -1,4 +1,20 @@
-def totalCost(days):
+from flask import Flask, render_template, request
 
-    totalPrice = days * 1750
-    return totalPrice
+def totalCost(days, mics, rooms, roomtime, courts, courttime, bbqtime, spots,tech):
+    TotalGreathall = days * 1750
+    if request.form.get('cleaning'):
+        TotalGreathall += 240
+    TotalGreathall += (tech * 35)
+    if request.form.get('kitchen'):
+        TotalGreathall += 500
+    TotalGreathall += (mics * 35)
+    if request.form.get('av'):
+        TotalGreathall += rooms * 15 * roomtime
+    else:
+        TotalGreathall += rooms * 10 * roomtime
+    TotalGreathall += courts * 20 * courttime
+    if request.form.get('bbq'):
+        TotalGreathall += 25 * bbqtime
+    TotalGreathall += (days*1000 + spots*10)
+
+    return TotalGreathall
